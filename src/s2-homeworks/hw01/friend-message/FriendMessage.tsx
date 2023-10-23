9db63f6b50d734ef898f10169c34d5231b77b77a
Subhonbek <basubhonbek@gmail.com>
2023-10-23 10:17:41 +0300

    login hm1

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -1,49 +1,25 @@
-import React from 'react'
-import s from './FriendMessage.module.css'
+import s from "./FriendMessage.module.css";
+import { MessageType } from "../HW1";
 
-// создать тип вместо any и отобразить приходящие данные
-const FriendMessage = (props: any) => {
-    return (
-        <div
-            id={'hw1-friend-message-' + props.message.id}
-            className={s.friendMessage}
-        >
-            <div className={s.friendImageAndText}>
-                <img
-                    id={'hw1-friend-avatar-' + props.message.id}
-                    // создаёт студент
-
-                    //
-                />
-                <div className={s.friendText}>
-                    <div
-                        id={'hw1-friend-name-' + props.message.id}
-                        className={s.friendName}
-                    >
-                        {/*создаёт студент*/}
-
-                        {/**/}
-                    </div>
-                    <pre
-                        id={'hw1-friend-text-' + props.message.id}
-                        className={s.friendMessageText}
-                    >
-                        {/*создаёт студент*/}
-
-                        {/**/}
-                    </pre>
-                </div>
-            </div>
-            <div
-                id={'hw1-friend-time-' + props.message.id}
-                className={s.friendTime}
-            >
-                {/*создаёт студент*/}
-
-                {/**/}
-            </div>
+const FriendMessage = ({ id, message, user }: MessageType) => {
+  return (
+    <div id={"hw1-friend-message-" + id} className={s.friendMessage}>
+      <div className={s.friendImageAndText}>
+        <img id={"hw1-friend-avatar-" + id} src={user.avatar} />
+        <div className={s.friendText}>
+          <div id={"hw1-friend-name-" + id} className={s.friendName}>
+            {user.name}
+          </div>
+          <pre id={"hw1-friend-text-" + id} className={s.friendMessageText}>
+            {message.text}
+          </pre>
         </div>
-    )
-}
+      </div>
+      <div id={"hw1-friend-time-" + id} className={s.friendTime}>
+        {message.time}
+      </div>
+    </div>
+  );
+};
 
-export default FriendMessage
+export default FriendMessage;
--- src/s2-homeworks/hw01/message-sender/MessageSender.tsx
+++ src/s2-homeworks/hw01/message-sender/MessageSender.tsx
@@ -1,76 +1,71 @@
-import React, {useEffect, useRef, useState} from 'react'
-import { message0 } from '../HW1'
-import s from './MessageSender.module.css'
+import { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from "react";
+import { MessageType, message0 } from "../HW1";
+import s from "./MessageSender.module.css";
 
-// компонента, которая тестирует вашу компоненту (не изменять, any не трогать)
-const MessageSender = (props: any) => {
-    const M = props.M
-    const textareaRef = useRef<HTMLTextAreaElement | null>(null);
-    const [messages, setMessages] = useState<any[]>([])
-    const [text, setText] = useState<any>('')
+type MessageSenderType = {
+  M: ({ id, message, user }: MessageType) => JSX.Element;
+};
 
-    const onChange = (e: any) => {
-        setText(e.currentTarget.value)
+const MessageSender = (props: MessageSenderType) => {
+  const M = props.M;
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+  const [messages, setMessages] = useState<MessageType[]>([]);
+  const [text, setText] = useState<string>("");
+
+  const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.currentTarget.value);
+  };
+
+  useEffect(() => {
+    if (textareaRef?.current) {
+      textareaRef.current.style.height = "0px";
+      textareaRef.current.style.height =
+        textareaRef.current.scrollHeight + "px";
     }
+  }, [text]);
 
-    useEffect(() => {
-        if (textareaRef?.current) {
-            textareaRef.current.style.height = '0px'
-            textareaRef.current.style.height = textareaRef.current.scrollHeight + 'px'
-        }
-    }, [text])
+  const addMessage = () => {
+    setMessages([
+      ...messages,
+      {
+        id: messages.length ? messages.length + 1 : 1,
+        user: message0.user,
+        message: {
+          text,
+          time: new Date().toTimeString().slice(0, 5),
+        },
+      },
+    ]);
+    setTimeout(() => setText(""), 4);
+  };
 
-    const addMessage = () => {
-        setMessages([
-            ...messages,
-            {
-                id: messages.length ? messages.length + 1 : 1,
-                user: message0.user,
-                message: {
-                    text,
-                    time: new Date().toTimeString().slice(0, 5),
-                },
-            },
-        ])
-        setTimeout(() => setText(''), 4)
-    }
+  const onKeyDown = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+    e.key === "Enter" && e.shiftKey && addMessage();
+  };
 
-    const onKeyDown = (e: any) => {
-        e.key === 'Enter' && e.shiftKey && addMessage()
-    }
+  return (
+    <>
+      {messages.map((m) => (
+        <M key={"message" + m.id} {...m} />
+      ))}
 
-    return (
-        <>
-            {messages.map((m) => (
-                <M key={'message' + m.id} message={m} />
-            ))}
+      <div id={"hw1-send-message-form"} className={s.sendForm}>
+        <textarea
+          id={"hw1-textarea"}
+          className={s.textarea}
+          ref={textareaRef}
+          title={"Shift+Enter for send"}
+          placeholder={"Type your message"}
+          value={text}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+        />
+        <button id={"hw1-button"} className={s.button} onClick={addMessage}>
+          Send
+        </button>
+      </div>
+    </>
+  );
+};
 
-            <div id={'hw1-send-message-form'} className={s.sendForm}>
-                <textarea
-                    id={'hw1-textarea'}
-                    className={s.textarea}
-                    ref={textareaRef}
-
-                    title={'Shift+Enter for send'}
-                    placeholder={'Type your message'}
-                    value={text}
-
-                    onChange={onChange}
-                    onKeyDown={onKeyDown}
-                />
-                <button
-                    id={'hw1-button'}
-                    className={s.button}
-
-                    onClick={addMessage}
-                >
-                    {/*текст кнопки могут изменить студенты*/}
-                    Send
-                    {/**/}
-                </button>
-            </div>
-        </>
-    )
-}
-
-export default MessageSender
+export default MessageSender;
